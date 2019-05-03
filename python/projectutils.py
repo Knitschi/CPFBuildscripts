217dc8828a2e70caca6c8e55b426d06792df4d97
Knitschi <knitschi@freenet.de>
2019-05-03 17:24:13 +0200

    #27 Changes file endings to rst.

---
+++ python/projectutils.py
@@ -0,0 +1,21 @@
+
+from . import miscosaccess
+
+
+
+def get_version_from_repository(cpfRootDir, package = None):
+    """
+    Runs the getVersionFromRepository.cmake in order to get the version of a package or build-project repository.
+    """
+    osa = miscosaccess.MiscOsAccess()
+    repo_dir = cpfRootDir
+    if package:
+        repo_dir.joinpath('Sources/{0}'.format(package))
+    script = cpfRootDir.joinpath('Sources/CPFCMake/Scripts/getVersionFromRepository.cmake')
+
+    return osa.execute_command_output(
+        'cmake -DREPO_DIR="{0}" -P {1}'.format(repo_dir, script),
+        cwd=cpfRootDir,
+        print_output=miscosaccess.OutputMode.ON_ERROR,
+        print_command=False
+    )[0]
