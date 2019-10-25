989f6ae44b134b0785839998530ae50fce02d392
Knitschi <11889116+Knitschi@users.noreply.github.com>
2019-10-25 20:29:53 +0200

    Fixes a bug in get_version_from_repository() that caused the function to always return the version of the ci project.

--- python/projectutils.py
+++ python/projectutils.py
@@ -7,10 +7,11 @@ def get_version_from_repository(cpfRootDir, package = None):
     """
     Runs the getVersionFromRepository.cmake in order to get the version of a package or build-project repository.
     """
+
     osa = miscosaccess.MiscOsAccess()
     repo_dir = cpfRootDir
     if package:
-        repo_dir.joinpath('Sources/{0}'.format(package))
+        repo_dir = repo_dir.joinpath('Sources/{0}'.format(package))
     script = cpfRootDir.joinpath('Sources/CPFCMake/Scripts/getVersionFromRepository.cmake')
 
     return osa.execute_command_output(
