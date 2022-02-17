0be28fa7338a34dca72b78c4b7475994610076b0
Knitschi <knitschi.public@freenet.de>
2022-02-17 21:37:16 +0100

    Fixes a bug in get_version_from_repository()

--- python/projectutils.py
+++ python/projectutils.py
@@ -3,7 +3,7 @@ from . import miscosaccess
 
 
 
-def get_version_from_repository(cpfRootDir, package = None):
+def get_version_from_repository(cpfRootDir, cpfCMakeDir, package = None):
     """
     Runs the getVersionFromRepository.cmake in order to get the version of a package or build-project repository.
     """
@@ -12,7 +12,7 @@ def get_version_from_repository(cpfRootDir, package = None):
     repo_dir = cpfRootDir
     if package:
         repo_dir = repo_dir.joinpath('Sources/{0}'.format(package))
-    script = cpfRootDir.joinpath('Sources/CPFCMake/Scripts/getVersionFromRepository.cmake')
+    script = cpfRootDir / cpfCMakeDir / 'Scripts/getVersionFromRepository.cmake'
 
     return osa.execute_command_output(
         'cmake -DREPO_DIR="{0}" -P "{1}"'.format(repo_dir, script),
